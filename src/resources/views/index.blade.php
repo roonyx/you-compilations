492ba8ac97cbec2fd0d931faa0f6d0f22617b186
Vladmir Pogarsky <tzendos@mail.ru>
2018-11-22 16:53:29 +0300

    Переводы текстов

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -41,7 +41,7 @@
 
         <div>
             <p>
-                YouCompilation непозволит пропустить видео на интересующую вас тематику. С помощью данного сервиса вы сможете получить подборки видео каждый день. Подборки состоят из тегов, теги в тоже время отображает интересующую вас информацию. А также, вы не сможете забыть о подборках, ибо система нотификации пришлет вам на почту напоминание.
+                YouCompilation will not allow you to skip videos on interesting topics. With this service you can get a collection of videos every day. Collections consist of tags. Tags displays the information you are interested in. If you forget about the collection, the notification system will send you a reminder.
             </p>
         </div>
 
@@ -53,9 +53,9 @@
                             <div class="animated wow animated" style="visibility: visible; animation-name: rollIn;">
                                 <span><i class="material-icons" style="font-size: 40px">open_in_new</i></span>
                             </div>
-                            <h3 class="animated fadeInUp wow animated" style="visibility: visible; animation-name: fadeInUp;">Просматривать свежий контент</h3>
+                            <h3 class="animated fadeInUp wow animated" style="visibility: visible; animation-name: fadeInUp;">New content</h3>
                             <p class="animated fadeInDown wow animated" style="visibility: visible; animation-name: fadeInDown;">
-                                Поиск осуществляется по новому и популярному контенту. Не бойтесь пропустить интересующее вас видео, сервис найдет его и покажем вам.
+                                Watch the latest videos. Do not be afraid to miss the video you are interested in. Service will find it and show you.
                             </p>
                         </div>
                     </div>
@@ -64,9 +64,9 @@
                             <div class="animated wow animated" style="visibility: visible; animation-name: rollIn;">
                                 <span><i class="material-icons" style="font-size: 40px">fiber_new</i></span>
                             </div>
-                            <h3 class="animated fadeInUp wow animated" style="visibility: visible; animation-name: fadeInUp;">Получать напоминания</h3>
+                            <h3 class="animated fadeInUp wow animated" style="visibility: visible; animation-name: fadeInUp;">Receive reminders</h3>
                             <p class="animated fadeInDown wow animated" style="visibility: visible; animation-name: fadeInDown;">
-                                Каждый день вы будете получать оповещение на почту с ссылкой и краткой информацией.
+                                Every day you will receive an email notification with a link and brief information.
                             </p>
                         </div>
                     </div>
@@ -75,9 +75,9 @@
                             <div class="animated wow animated" style="visibility: visible; animation-name: rollIn;">
                                 <span><i class="material-icons" style="font-size: 40px">share</i></span>
                             </div>
-                            <h3 class="animated fadeInUp wow animated" style="visibility: visible; animation-name: fadeInUp;">Делиться с друзьями</h3>
+                            <h3 class="animated fadeInUp wow animated" style="visibility: visible; animation-name: fadeInUp;">Share with friends</h3>
                             <p class="animated fadeInDown wow animated" style="visibility: visible; animation-name: fadeInDown;">
-                                Вы сможете делиться подборками с друзьями и близкими отправив им ссылку.
+                                You can share the collections with your friends and family by sending them a link.
                             </p>
                         </div>
                     </div>
@@ -86,17 +86,11 @@
         </div>
 
         <div>
-            <span>
-
-            </span>
-        </div>
-
-        <div>
             <div class="title">
                 <h2>For what?</h2>
             </div>
             <p>
-                Не всем удаётся уделять большое количества времени на YouTube, также мы не можем знать обо всех интересных каналах и теряем большое количество интересной информации. Наш сервис, это окно в мир актуальной и популярной информации, окно с свежими видео которые вам должны понравиться, ведь вся найденная информация отражает введенные в настройках теги.
+                Not everyone is able to devote a lot of time on YouTube. Also, we can't know about all the interesting channels and lose a lot of interesting information. Our service is a window into the world of current and popular information. A window with fresh videos that you should like, because all the information found reflects the tags entered in the settings.
             </p>
         </div>
         <div>
@@ -104,7 +98,7 @@
                 <h2>What's next?</h2>
             </div>
             <p>
-                После регистрации вам нужно заполнить теги в настройках пользователя. Выберите теги с помощью поисковика и умного подсказчика, либо если тега не существует напишите их через запятые.
+                After registration you need to fill in the tags in the user settings. Select tags with the help of a search engine and a smart tooltip. If the tag does not exist write them separated by commas.
             </p>
         </div>
     </div>
@@ -122,7 +116,6 @@
                                 <img class="card-img-top"
                                      src="{{ !empty($image = \App\Models\Compilations\Compilation::prettyImage($compilation)) ? $image['url']  : 'no' }}"
                                      alt="Card image cap">
-
                                 <div class="overlay">
                                     Date: {{ $compilation->created_at->toDateString() }}
                                 </div>
