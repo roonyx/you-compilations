90a788492531b6e911952605d29b1c18a42598e9
Vladmir Pogarsky <tzendos@mail.ru>
2018-12-06 12:58:42 +0300

    Удаление лишних точек входа

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 Route::get('tags/{name?}', 'Compilations\TagController@index')->name('api_tags_search');
